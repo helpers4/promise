31757dea79388bfbae37199f8e05e5e224c9039c
Bérenger <berenger@arnaud.xyz>
2022-09-05 23:30:37 +0200

    chore: :memo: fix doc namespace

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+/*
+ * This program is under the terms of the GNU Affero General Public License version 3
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+/**
+ * Library of promise helpers.
+
+ * @packageDocumentation
+ * @module promise
+ * @preferred
+ */
+
 export * from './helper/promise';
